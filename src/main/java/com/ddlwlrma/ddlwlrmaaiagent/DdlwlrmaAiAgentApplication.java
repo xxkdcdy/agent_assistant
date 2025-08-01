0654c58b8431e51277aa01a9c2c4611e497c8770
Chan dy <dychan2000@163.com>
2025-08-01 21:11:47 +0800

    Initial commit

---
+++ src/main/java/com/ddlwlrma/ddlwlrmaaiagent/DdlwlrmaAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.ddlwlrma.ddlwlrmaaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdlwlrmaAiAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdlwlrmaAiAgentApplication.class, args);
+	}
+
+}
